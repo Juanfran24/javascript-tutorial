8e3c80c076e086a2f2c8761629d682f82dbca661
juanfran24 <xxxestebanfrancoxxx@gmail.com>
2022-08-09 15:31:25 -0500

    exersice 3 finished

---
+++ 3-fetch-api-ajax/fetch.js
@@ -0,0 +1,24 @@
+const getAuthorCommitier = async () => {
+  const author = await fetch('https://api.github.com/repos/pushdev-code/javascript-tutorial/commits');
+  const authorJson = await author.json();
+  return authorJson;
+}
+
+
+const showCommitier = async () => {
+  const arrCommitiers = await getAuthorCommitier();
+  let html = `
+    <ul>
+      ${arrCommitiers.map(author => `
+        <li>
+          <h2>${author.author!=null && author.author.login}</h2>
+          <img src="${author.author!=null && author.author.avatar_url}" alt="${author.author!=null && author.author.login}">
+        </li>
+      `).join('')}
+    </ul>
+  `;
+  const body = document.querySelector('body');
+  body.innerHTML = html;
+}
+
+showCommitier();
